05053a2213c713a6dfc32fecc3469c9a0f174a5a
Joshua Taguicana <joshtaguicana@gmail.com>
2020-02-02 16:47:39 -0500

    created landing page for game

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import Main from "./pages/main"
-
-import './App.css';
-
+import React from "react";
+import Main from "./pages/main";
+import Landing from "./pages/Landing";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-      <Main />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/play" component={Main} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Landing/Landing.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./Landing.scss";
+import dogSVG from "../../assets/dog.svg";
+import hotdogSVG from "../../assets/hot-dog.svg";
+
+const apiKey = process.env.REACT_APP_GIPHY_API_KEY;
+
+export default function Landing() {
+  const [dogImg1, setDogImg1] = useState(
+    "https://media0.giphy.com/media/3oEduFodl3UbrrHCfe/giphy.gif?cid=dc4fc341cc811f7bf59e48da2ca8f62e8b0092482a97ce8f&rid=giphy.gif"
+  );
+  const [dogImg2, setDogImg2] = useState(
+    "https://media1.giphy.com/media/xTiTnJ8sYG1l9bSj0k/giphy-preview.webp?cid=dc4fc341a61629451d05608553ee120082fc3a903d538078&rid=giphy-preview.webp"
+  );
+  const [hotDogImg1, setHotDogImg1] = useState(
+    "https://media0.giphy.com/media/UrQHy8pEdKBRzejqxv/giphy.gif?cid=dc4fc3411587b8bb5ef52bba50792c4aac64ecaf6724d3a7&rid=giphy.gif"
+  );
+  const [hotDogImg2, setHotDogImg2] = useState(
+    "https://media3.giphy.com/media/jAV63tmDsCuo8/giphy-downsized-medium.gif?cid=dc4fc3417cd520d6d2e482f9524adbb71d63c0b1629abc2f&rid=giphy-downsized-medium.gif"
+  );
+
+  // useEffect(() => {
+  //     getData("dog", setDogImg1, 1800);
+  //     getData("dogs", setDogImg2, 4000);
+  //     getData("hot%20dog", setHotDogImg1, 3400);
+  //     getData("hot%20dogs", setHotDogImg2, 1200);
+  //   }, []);
+
+  const getData = (item, setItem, time) => {
+    setInterval(() => {
+      axios
+        .get(
+          `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${item}`
+        )
+        .then(res => {
+          setItem(res.data.data.images.downsized_medium.url);
+        });
+    }, time);
+  };
+
+  return (
+    <div className="landing">
+      <img className="dog-img" src={dogSVG} alt="Dog" />
+      <img className="hot-dog-img-one" src={hotdogSVG} alt="Hot Dog" />
+      <img className="hot-dog-img-two" src={hotdogSVG} alt="Hot Dog" />
+      <img className="hot-dog-img-three" src={hotdogSVG} alt="Hot Dog" />
+      <img className="hot-dog-img-four" src={hotdogSVG} alt="Hot Dog" />
+      <img className="hot-dog-img-five" src={hotdogSVG} alt="Hot Dog" />
+      <img className="hot-dog-img-six" src={hotdogSVG} alt="Hot Dog" />
+      <h1 className="landing__title">
+        <span className="landing__title--anim-one">DAWG </span>
+        <span className="landing__title--anim-two">OR </span>
+        <span className="landing__title--anim-three">HAWT DAWG?!?</span>
+      </h1>
+      <Link to="/play">PLAY!</Link>
+      {/* <div className="landing__img-container">
+        <img className="landing__imgs" src={dogImg1} alt="dog gif" />
+        <img className="landing__imgs" src={hotDogImg1} alt="hot dog gif" />
+        <img className="landing__imgs" src={hotDogImg2} alt="hot dog gif" />
+        <img className="landing__imgs" src={dogImg2} alt="dog gif" />
+      </div> */}
+    </div>
+  );
+}
