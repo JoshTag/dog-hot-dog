80dfa8c66c5f066574ff6c0e93ed5c9fac5285dd
Roy Lee <royleejr@Roys-MacBook-Pro.local>
2020-02-05 20:28:12 -0500

    styled highscore page to be responsive

--- client/src/pages/Highscore/Highscore.js
+++ client/src/pages/Highscore/Highscore.js
@@ -4,14 +4,25 @@ import Scores from '../../components/Scores/Scores';
 
 import './Highscore.scss'
 
-const array = [1,2,3,4,5,6,7,8,9]
+const array = [1,2,3,4,5,6,7,8,9,10]
 
 export default class Highscore extends React.Component {
+  state = {
+    class: null
+  }
+
+  componentDidMount() {
+    setTimeout(()=> {
+      this.setState({
+        class: 'animated rollIn'
+      })
+    },100)
+  }
   render() {
     return (
       <section className="highscore">
         <div className="highscore__container animated bounceInDown">
-          <h1 className="highscore__title">HIGHSCORES</h1>
+          <h1 className={`highscore__title ${this.state.class}`}>HIGHSCORES</h1>
           <div className="highscore__list">
             {
               array.map(item => {
@@ -22,7 +33,7 @@ export default class Highscore extends React.Component {
         </div>
         <div className="highscore__button-container">
           <button className="highscore__play-again">Play Again</button>
-          <button className="highscore__play-again">Play Again</button>
+          {/* <button className="highscore__play-again">Play Again</button> */}
         </div>
       </section>
     )
